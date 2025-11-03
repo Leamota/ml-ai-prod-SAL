6fc1a51c12b647e7febed69499b3ba84e5eab6de
Saika118 <151307408+Saika118@users.noreply.github.com>
2025-11-02 20:25:47 -0500

    Create test_schema_validation.py

---
+++ tests/test_schema_validation.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from stream.schema_validation import rating_schema
+import pytest
+
+def test_schema_valid():
+    df = pd.DataFrame({
+        "userId": [1, 2],
+        "movieId": [10, 20],
+        "rating": [4.5, 3.0],
+        "timestamp": [123456, 654321]
+    })
+    rating_schema.validate(df)
